126fc3d2929c5d46cfa39e6ec23ff33fff5fec5c
Flatiron School <leslie.hsu@flatironschool.com>
2018-04-02 20:15:43 -0400

    fixed redux error

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
 
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f))
+
+
 
 
 ReactDOM.render(
