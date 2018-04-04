96e0c0b715a4d4bab82471c34a92006bc8ccce87
Flatiron School <leslie.hsu@flatironschool.com>
2018-04-04 13:21:32 -0400

    minor fixes

--- src/URL.js
+++ src/URL.js
@@ -1,3 +1,12 @@
-export const URL_ROOT_API = "https://togetherbackend.herokuapp.com/api/v1/"
+//FOR HOSTING ON HEROKU
 
+export const URL_ROOT_API = "https://togetherbackend.herokuapp.com/api/v1/"
 export const URL_ROOT_BASE = "https://togetherbackend.herokuapp.com/"
+
+
+
+
+// REPLACE ABOVE WITH BELOW IF YOU ARE HOSTING LOCALLY
+// export const URL_ROOT_API = "http://localhost:3000/api/v1/"
+//
+// export const URL_ROOT_BASE = "http://localhost:3000/"
